031977966fccaff14dbc684c3a10c8d7215d39be
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-05-09 18:03:45 +0200

    Add: extra calculations

--- public/js/script.js
+++ public/js/script.js
@@ -3,17 +3,28 @@
   const socket = io();
 
   let actualAmount = 0;
+  let generatedEnergy = 20; // Hard coded for now (later actual via Socket)
+  let totalGoal = 1680;
+  let dayGoal = 400;
+
+  let dayPercentage = 0;
+  let totalPercentage = 0;
 
   const generateData = {
     energyUse() {
       setInterval(function() {
         let totalAmountPerSecond = 0.375;
         actualAmount = actualAmount + totalAmountPerSecond;
-        console.log(actualAmount);
+        console.log(`${actualAmount} kWh energy used`);
+        generateData.calculatePercentage();
+        console.log(`${totalPercentage}% of total goal reached`);
         if(actualAmount >= 10000) {
           actualAmount = actualAmount;
         }
       }, 1000);
+    },
+    calculatePercentage() {
+      totalPercentage = (actualAmount / totalGoal * 100).toFixed(2);
     }
   }
 
