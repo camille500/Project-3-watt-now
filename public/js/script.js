f51d21acf4484dcb1d55dec521e295aa6a8e5218
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-05-09 17:22:15 +0200

    Auto generated kWh usage

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,19 @@
 (function() {
 
-  var socket = io();
+  const socket = io();
+
+  let actualAmount = 0;
+
+  const generateData = {
+    energyUse() {
+      setInterval(function() {
+        let totalAmountPerSecond = Math.floor((Math.random() * 10) + 1);
+        actualAmount = actualAmount + totalAmountPerSecond;
+        console.log(actualAmount);
+      }, 1000);
+    }
+  }
+
+  generateData.energyUse()
 
 }());
