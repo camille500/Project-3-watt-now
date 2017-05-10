b5cfa9c2b10748a26053994cf72cfd53adb7186a
Zishrodrigues <zish.rodrigues@gmail.com>
2017-05-10 16:46:04 +0200

    fixed d3

--- web/public/js/datavisualization.js
+++ web/public/js/datavisualization.js
@@ -1,75 +1,86 @@
-// SVG element
-const height = 400,
-      width = 600,
-    svg = d3.select('main')
-      .append('svg')
-      .attr('height', height)
-      .attr('width', width);
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+     width = 900 - margin.left - margin.right,
+     height = 500 - margin.top - margin.bottom;
 
-// Create the Scale we will use for the Axis
-const xAxisScale = d3.scale.ordinal().rangeRoundBands([0, 700], 0.05); //ordinal for discrete domains such as names or in my case roadNames
-const yAxisScale = d3.scale.linear().range([height, 0]); // linear is used for the quantitative scales -> continuous domain such as numbers or in my case km
+// set the ranges
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+var y = d3.scale.linear().range([height, 0]);
+ // define the axis
+ var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
 
-//Create the Axis
-const xAxis = d3.svg.axis()
-  .scale(xAxisScale)
-  .orient('bottom')
-  .ticks(10);
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
 
-const yAxis = d3.svg.axis()
-  .scale(yAxisScale)
-  .orient('left')
-  .ticks(20);
+// add the SVG element
+var svg = d3.select("main").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-d3.json('http://localhost:3000/js/moeder.json', (err, data) => {
-  // Set the ticks
-  console.log(data);
+// load the data
+d3.json("/js/data.json", function(error, data) {
 
-  // xAxisScale.domain(data.forEach((d) => d.deDatering)); //d.road
-  // yAxisScale.domain([0, d3.max(data, (d) => d.onzeData)]); //d.dag
+   data.forEach(function(d) {
+        d.Letter = d.Letter;
+        d.KW = +d.KW;
+    });
 
-  // Append the y axis
-  svg.append('g')
-    .attr('class', 'yAxis')
-    .call(yAxis);
+ // scale the range of the data
+  x.domain(data.map(function(d) { return d.Letter; }));
+  y.domain([0, d3.max(data, function(d) { return d.KW; })]);
 
-  // Append the x axis
-  svg.append('g')
-    .attr('class', 'xAxis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis)
-  .selectAll('text') // apend text labels to ticks
-    .attr('dx', '-4em')
-    .attr('dy', '-.55em')
-    .attr('transform', 'rotate(-60)');
+ // add axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
 
-  function update(time) {
-      var bars = svg.selectAll('rect').data(data);
+ svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 5)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("KW");
 
-      bars.enter(data).append('rect');// lopen door data en append rect voor elk data item
-      bars.exit().remove(); // checkt of er items zijn verwijderd uit dataset, zo ja verwijderd die ook rects.
+   var bars = svg.selectAll('bar').data(data);
+        bars.enter(data).append('rect');// lopen door data en append rect voor elk data item
+        bars
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.Letter); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.KW); })
+          .attr("height", function(d) { return height - y(d.KW); });
 
-      bars
-        .attr('class', time)
-        .attr('x', (d) => xAxisScale(d.deDatering))
-        .attr('width', xAxisScale.rangeBand())
-        .attr('y', function(d) {
-          if (time === 'day') {
-            return yAxisScale(d.onzeData);
-          }
-        })
-        .attr('height', function(d) {
-          if (time === 'day') {
-            return height - yAxisScale(d.overdag);
-          }
-        });
+   function updateMyData(data) {
+      var refresh = svg.selectAll('.bar')
+        .data(data);
+
+     refresh.exit()
+        .remove();
+
+     refresh.enter()
+        refresh.enter(data).append('rect')
+        .attr("x", function(d) { return x(d.Letter); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.KW); })
+        .attr("height", function(d) { return height - y(d.KW); });
     }
 
-  update('day');
-
-  var time = document.getElementById('timeSelector');
-
-  // time.addEventListener('change', function() {
-  //   update(time.value);
-  // });
+   setInterval(function() {
+      updateMyData(data);
+      console.log('updatedddddd');
+    }, 5000);
 });
