dd65a16bb9856ee4d24f89fdfd5d17027a58a98f
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-05-12 17:24:52 +0200

    Forgot to post 1 LUA file on Github

--- Lua Code/init.lua
+++ Lua Code/init.lua
@@ -5,48 +5,96 @@ local socketmodule = require 'socketmodule'
 
 function init()
 
-  ws2812.init()
- local i, buffer = 0, ws2812.newBuffer(8, 3)
- 
- function startStrip(colorArray)
-   buffer:fill(colorArray[1], colorArray[2], colorArray[3])
-   ws2812.write(buffer)
- end
- 
- startStrip({220, 0, 0})
+  buffercount = 0
+  time = 0
+  firstOn = false
+  color = 0
+  condition = false
 
-  print('init')
-  print(wifi.sta.getip())
+
+  conditiontimer = tmr.create()
+  conditiontimer:register(10000, tmr.ALARM_AUTO, function()
+    print(condition)
+    if(condition == true) then
+     condition = false
+    else
+     buffercount = 0
+     firstOn = false
+     blinktimer:stop()
+     mytimer:stop()
+     startStrip({0, 0, 0})
+    end
+  end)
+  conditiontimer:interval(5000)
+
+  conditiontimer:start()
+
+  mytimer = tmr.create()
+  mytimer:register(10000, tmr.ALARM_AUTO, function()
+    buffercount = buffercount + 1
+    if(buffercount < 9) then
+      buffer:set(buffercount, string.char(255, 0, 0))
+      ws2812.write(buffer)
+    else
+      startStrip({0, 0, 0})
+      buffercount = 0
+    end
+    time = 0
+    print(time)
+  end)
+  mytimer:interval(10000) -- actually, 3 seconds is better!
+
+  blinktimer = tmr.create()
+  blinktimer:register(10000, tmr.ALARM_AUTO, function()
+    print(color)
+    if(color == 255) then
+      color = 0
+    else
+      color = 255
+    end
+    local blinkbuffer = buffercount + 1
+    if(blinkbuffer < 9) then
+      buffer:set(blinkbuffer, string.char(0, 0, color))
+      ws2812.write(buffer)
+    end
+  end)
+  blinktimer:interval(1000)
+  b = 255
+  r = 0
+
+  ws2812.init()
+  i, buffer = 0, ws2812.newBuffer(8, 3)
+  print(buffer)
+
+  function startStrip(colorArray)
+    buffer:fill(colorArray[1], colorArray[2], colorArray[3])
+    ws2812.write(buffer)
+  end
+
+
+  startStrip({0, 0, 0})
 
   local ws = socketmodule.start()
 
   ws:on('connection', function(ws)
     print('Websocket has Connected!')
-   
   end)
 
-  ws:on('close', function(_, status)
-    print('Websocket closed the connection', status)
-    ws = nil
+  ws:on('receive', function(_, message)
+    condition = true
+    print(firstOn)
+    print(buffercount)
+    if(firstOn == false) then
+      firstOn = true
+      blinktimer:start()
+    end
+    if(time == 0) then
+      mytimer:start()
+      time = 1
+    end
+    print('recieved')
   end)
 
-clicks = 0
-counter = 0
-
-function onChange()
-   clicks = clicks + 1
-   startStrip({0, 0, 255})
-   if(clicks == 3) then
-    clicks = 0
-    counter = counter + 10
-    ws:send(counter)
-    print('Yep')
-   end 
-end
-
-  gpio.mode(1, gpio.INT, gpio.PULLUP)
-  gpio.trig(1, 'both', onChange)
---  
 end
 
 
